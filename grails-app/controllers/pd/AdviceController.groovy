cb4bbcf1225c9fbb260c014709727f4fa80e2ec2
沈鸿飞 <shf19961008@163.com>
2017-06-03 00:23:07 +0800

    功能基本实现

---
+++ grails-app/controllers/pd/AdviceController.groovy
@@ -0,0 +1,24 @@
+package pd
+import grails.gorm.transactions.Transactional
+
+
+@Transactional
+class AdviceController {
+	static responseFormats = ['json']
+	
+    def submit(){
+        def student = session.student
+        new Advice(content:params.content,advisor:student).save()
+        render(1)
+    }
+    
+    def list(){
+        def page        = (params.pageIndex?:0) as Integer
+        def size        = (params.pageSize?:5) as Integer
+        def sortParams  = (params.sort?:'id,desc').split(',') as List
+        def sortBy      = sortParams[0]
+        def order       = sortParams[1]
+        def advices = Advice.findAll("from Advice as advice inner join fetch advice.advisor order by advice.$sortBy $order".toString(),[max:size,offset:page*size])
+        render view:'/mypage',model:[myPage:new MyPage(advices,Advice.count,size,page),template:'/advice/details']
+    }
+}
